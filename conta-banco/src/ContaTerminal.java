e243ec6ae595a3398b7d4f2384c287cd8087f291
vander-son <vanderson24.vs@gmail.com>
2023-09-25 20:11:47 -0300

    projeto conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-     System.out.println("conta bancaria");
+     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+     System.out.println("Digita sua Conta ");
+     String Conta = scanner.next();
+
+     System.out.println("Digita sua Agencia ");
+     String Agencia = scanner.next();
+
+    
+
+     String nomeCompleto = "VANDERSON" + "AZEVEDO ";
+
+     System.out.println("NomeCompleto");
+     String NomeCompleto = scanner.next();
+
+     System.out.println ("Olá [VANDERSON AZEVEDO ], obrigado por criar uma conta em nosso banco, sua agência é [389-1], conta [1020] e seu saldo [10000.5] já está disponível para saque");
     }   
 }
